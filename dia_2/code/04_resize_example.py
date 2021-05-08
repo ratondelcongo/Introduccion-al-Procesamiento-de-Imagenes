4385382fa067af866d2d6ec70a64cb42fa8884a2
Luigi Layme <35671380+ratondelcongo@users.noreply.github.com>
2021-05-08 13:39:12 -0500

    dia 2 commit

--- dia_2/code/04_resize_example.py
+++ dia_2/code/04_resize_example.py
@@ -4,7 +4,11 @@ src = 'src/img/perros.png'
 
 img = cv2.imread(src)
 
-dim = (200, 300)
+aspect_ratio = img.shape[0]/img.shape[1]
+
+print(aspect_ratio)
+
+dim = (200, int(200*aspect_ratio))
 resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
 cv2.imshow('img', img)
